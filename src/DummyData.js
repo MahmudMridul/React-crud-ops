3870eb027ec5fb103d81d9fd540395a05322fbb2
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-02-05 21:42:58 +0600

    File for dummy data

---
+++ src/DummyData.js
@@ -0,0 +1,27 @@
+export const todos = [
+    {
+        id: 1,
+        title: 'Do one thing',
+        shortDesc: 'Do that one thing for more than one hundred thousands times'
+    },
+    {
+        id: 2,
+        title: 'Do one thing',
+        shortDesc: 'Do that one thing'
+    },
+    {
+        id: 3,
+        title: 'Do one thing',
+        shortDesc: 'Do that one thing'
+    },
+    {
+        id: 4,
+        title: 'Do one thing',
+        shortDesc: 'Do that one thing'
+    },
+    {
+        id: 5,
+        title: 'Do one thing',
+        shortDesc: 'Do that one thing'
+    },
+]
